d9833047b49cf4f7dd603370d154a33323398e0b
magnitola <nikita.lukyanow@gmail.com>
2019-09-19 19:11:07 +0300

    “razdva”

---
+++ тРПО/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace тРПО
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FilePath = new System.Windows.Forms.TextBox();
+            this.path = new System.Windows.Forms.Button();
+            this.TextBox = new System.Windows.Forms.TextBox();
+            this.Write = new System.Windows.Forms.Button();
+            this.tablo = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // FilePath
+            // 
+            this.FilePath.Location = new System.Drawing.Point(13, 13);
+            this.FilePath.Name = "FilePath";
+            this.FilePath.ReadOnly = true;
+            this.FilePath.Size = new System.Drawing.Size(314, 20);
+            this.FilePath.TabIndex = 0;
+            // 
+            // path
+            // 
+            this.path.Location = new System.Drawing.Point(334, 11);
+            this.path.Name = "path";
+            this.path.Size = new System.Drawing.Size(52, 23);
+            this.path.TabIndex = 1;
+            this.path.Text = "...";
+            this.path.UseVisualStyleBackColor = true;
+            this.path.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // TextBox
+            // 
+            this.TextBox.Location = new System.Drawing.Point(13, 40);
+            this.TextBox.Name = "TextBox";
+            this.TextBox.Size = new System.Drawing.Size(314, 20);
+            this.TextBox.TabIndex = 2;
+            // 
+            // Write
+            // 
+            this.Write.Location = new System.Drawing.Point(334, 38);
+            this.Write.Name = "Write";
+            this.Write.Size = new System.Drawing.Size(52, 23);
+            this.Write.TabIndex = 3;
+            this.Write.Text = "Write";
+            this.Write.UseVisualStyleBackColor = true;
+            this.Write.Click += new System.EventHandler(this.Write_Click);
+            // 
+            // tablo
+            // 
+            this.tablo.Location = new System.Drawing.Point(13, 67);
+            this.tablo.Multiline = true;
+            this.tablo.Name = "tablo";
+            this.tablo.Size = new System.Drawing.Size(373, 176);
+            this.tablo.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(396, 255);
+            this.Controls.Add(this.tablo);
+            this.Controls.Add(this.Write);
+            this.Controls.Add(this.TextBox);
+            this.Controls.Add(this.path);
+            this.Controls.Add(this.FilePath);
+            this.Name = "Form1";
+            this.Text = "Журнал";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox FilePath;
+        private System.Windows.Forms.Button path;
+        private System.Windows.Forms.TextBox TextBox;
+        private System.Windows.Forms.Button Write;
+        private System.Windows.Forms.TextBox tablo;
+    }
+}
+
---
+++ тРПО/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace тРПО
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            CheckFileAsync();
+        }
+        public async void CheckFileAsync()
+        {
+            await
+                Task.Run(() => CheckFile());
+        }
+        private void CheckFile()
+        { 
+            DateTime a = DateTime.MinValue;
+            while (true)
+            {
+                try
+                {
+                    string pt = FilePath.Text;
+                    if (pt != String.Empty)
+                    {
+                        FileInfo s = new FileInfo(pt);
+                        if (s.LastWriteTime > a)
+                        {
+                            tablo.Text = "";
+                            using (StreamReader stream = new StreamReader(FilePath.Text))
+                            {
+                                while(!stream.EndOfStream)
+                                    tablo.Text+=stream.ReadLine() + Environment.NewLine;
+                            }
+                        }
+                        a = s.LastWriteTime;
+                        s = null;
+                    }
+                    
+                }
+                catch(Exception)
+                {
+
+                }
+                System.Threading.Thread.Sleep(500);
+            }
+        }
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+                FilePath.Text = ofd.FileName;
+            
+        }
+
+        private void Write_Click(object sender, EventArgs e)
+        {
+            if (TextBox.Text != String.Empty)
+            {
+                string s = TextBox.Text;
+                TextBox.Text = "";
+
+                StreamWriter sw = new StreamWriter(FilePath.Text, true);
+                sw.WriteLine($"{DateTime.Now} | {Environment.UserName} | {s}");
+                sw.Close();
+            }
+        }
+    }
+}
